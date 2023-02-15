0f8f90a96db0acff220f4fabf926605565c746d5
kasra paketeghad <kasrapak69@gmail.com>
2023-02-15 22:39:33 +0330

    Migrate from styled-components to emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import Container from "@mui/material/Container";
 
 export default function Home() {
-  return <div>here we are</div>;
+  return <Container maxWidth="sm">here we are</Container>;
 }
