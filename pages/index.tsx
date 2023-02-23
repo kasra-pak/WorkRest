a71e83ebd57e8179e4bd9b20bd7dbf8337dbdd52
kasra paketeghad <kasrapak69@gmail.com>
2023-02-23 11:07:24 +0330

    Add ThemeProvider & Header to the Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
+import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 
 import useCountdown from "@/hooks/useCountdown";
+import Header from "@/layouts/Header";
 import ToggleBtn from "@/components/ToggleBtn";
 import Countdown from "@/components/Countdown";
+
+import { theme } from "@/theme";
 
 export default function Home() {
   const { seconds } = useCountdown(240);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="sm">
+        <Header />
         <ToggleBtn />
         <Countdown seconds={seconds} />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
