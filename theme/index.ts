197d03beb289a81d77da5e41f4791c78ebcda9dd
kasra paketeghad <kasrapak69@gmail.com>
2023-02-23 11:05:30 +0330

    Create custom MUI theme

---
+++ theme/index.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#d60000",
+    },
+    secondary: {
+      main: "#388e3c",
+    },
+  },
+});
+
+export { theme };
